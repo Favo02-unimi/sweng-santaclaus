53a87401eb0d6a09b909cd219c5dab5af21a65ed
Luca Favini <luca.favini.02@gmail.com>
2024-12-26 02:25:34 +0100

    ROSSO: added add/get single toy to state

--- src/main/java/it/unimi/di/sweng/lab12/model/State.java
+++ src/main/java/it/unimi/di/sweng/lab12/model/State.java
@@ -1,6 +1,5 @@
 package it.unimi.di.sweng.lab12.model;
 
-
 import it.unimi.di.sweng.lab12.model.objects.City;
 import it.unimi.di.sweng.lab12.model.objects.Quantity;
 import it.unimi.di.sweng.lab12.model.objects.Toy;
@@ -9,9 +8,14 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public interface IState {
+public class State implements IState {
+    @Override
+    public void addToy(@NotNull Toy toy, @NotNull Quantity qty, @NotNull City city) {
 
-    public void addToy(@NotNull Toy toy, @NotNull Quantity qty, @NotNull City city);
+    }
 
-    public @NotNull List<ToyInfo> getToys();
+    @Override
+    public @NotNull List<ToyInfo> getToys() {
+        return null;
+    }
 }
