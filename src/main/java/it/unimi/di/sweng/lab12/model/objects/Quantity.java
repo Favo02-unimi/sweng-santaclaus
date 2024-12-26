eba5044234060d29946e5ce7af76f3685a30d799
Luca Favini <luca.favini.02@gmail.com>
2024-12-26 02:11:53 +0100

    VERDE: passed Quantity validity tests

--- src/main/java/it/unimi/di/sweng/lab12/model/objects/Quantity.java
+++ src/main/java/it/unimi/di/sweng/lab12/model/objects/Quantity.java
@@ -1,3 +1,12 @@
 package it.unimi.di.sweng.lab12.model.objects;
 
-public record Quantity(int qty) {}
+public record Quantity(int qty) {
+    public Quantity {
+        if (qty <= 0) {
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+        if (qty > 100) {
+            throw new IllegalArgumentException("Quantity must be less or equal than 100");
+        }
+    }
+}
