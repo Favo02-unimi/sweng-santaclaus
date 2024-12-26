b5a8af74e03086bfd2cc807ca77eb990d8f43dc5
Luca Favini <luca.favini.02@gmail.com>
2024-12-26 02:29:36 +0100

    VERDE: passed add/get single toy to state

--- src/main/java/it/unimi/di/sweng/lab12/model/State.java
+++ src/main/java/it/unimi/di/sweng/lab12/model/State.java
@@ -1,21 +1,32 @@
 package it.unimi.di.sweng.lab12.model;
 
-import it.unimi.di.sweng.lab12.model.objects.City;
-import it.unimi.di.sweng.lab12.model.objects.Quantity;
-import it.unimi.di.sweng.lab12.model.objects.Toy;
-import it.unimi.di.sweng.lab12.model.objects.ToyInfo;
+import it.unimi.di.sweng.lab12.model.objects.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class State implements IState {
+
+    private final @NotNull Map<ToyCity, Quantity> toys;
+
+    public State() {
+        toys = new HashMap<>();
+    }
+
     @Override
     public void addToy(@NotNull Toy toy, @NotNull Quantity qty, @NotNull City city) {
-
+        toys.put(new ToyCity(toy, city), qty);
     }
 
     @Override
     public @NotNull List<ToyInfo> getToys() {
-        return null;
+        List<ToyInfo> res = new ArrayList<>();
+        for (Map.Entry<ToyCity, Quantity> entry : toys.entrySet()) {
+            res.add(new ToyInfo(entry.getKey().toy(), entry.getValue(), entry.getKey().city()));
+        }
+        return res;
     }
 }
