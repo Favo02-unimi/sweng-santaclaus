ad4daf312f111856900aea2fb4a9edf94d161e10
Luca Favini <luca.favini.02@gmail.com>
2024-12-26 02:44:20 +0100

    VERDE: passed call action on InputPresenter

--- src/main/java/it/unimi/di/sweng/lab12/presenter/CInputPresenter.java
+++ src/main/java/it/unimi/di/sweng/lab12/presenter/CInputPresenter.java
@@ -1,16 +1,27 @@
 package it.unimi.di.sweng.lab12.presenter;
 
 import it.unimi.di.sweng.lab12.model.Model;
+import it.unimi.di.sweng.lab12.model.objects.City;
+import it.unimi.di.sweng.lab12.model.objects.Quantity;
+import it.unimi.di.sweng.lab12.model.objects.Toy;
 import org.jetbrains.annotations.NotNull;
 
 public class CInputPresenter implements InputPresenter {
 
+    private final @NotNull Model model;
 
     public CInputPresenter(@NotNull Model model) {
+        this.model = model;
     }
 
     @Override
-    public void action(@NotNull String text, @NotNull String text1) {
+    public void action(@NotNull String toyQty, @NotNull String cityStr) {
+        String[] token = toyQty.split(":");
 
+        Toy toy = new Toy(token[0]);
+        Quantity qty = Quantity.fromString(token[1]);
+        City city = new City(cityStr);
+
+        model.addToy(toy, qty, city);
     }
 }
