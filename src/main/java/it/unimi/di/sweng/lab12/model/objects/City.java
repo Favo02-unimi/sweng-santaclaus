666bcbe8747f1bff4de3ec4c244615ced7a524bf
Luca Favini <luca.favini.02@gmail.com>
2024-12-26 02:17:21 +0100

    VERDE: passed City validity tests

--- src/main/java/it/unimi/di/sweng/lab12/model/objects/City.java
+++ src/main/java/it/unimi/di/sweng/lab12/model/objects/City.java
@@ -2,4 +2,10 @@ package it.unimi.di.sweng.lab12.model.objects;
 
 import org.jetbrains.annotations.NotNull;
 
-public record City(@NotNull String name) {}
+public record City(@NotNull String name) {
+    public City {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be blank");
+        }
+    }
+}
