ef5b9702875383174533f34277c78bc4ff2e4f8f
Luca Favini <luca.favini.02@gmail.com>
2024-12-26 02:06:03 +0100

    VERDE: passed Toy validity tests

--- src/main/java/it/unimi/di/sweng/lab12/model/objects/Toy.java
+++ src/main/java/it/unimi/di/sweng/lab12/model/objects/Toy.java
@@ -2,4 +2,10 @@ package it.unimi.di.sweng.lab12.model.objects;
 
 import org.jetbrains.annotations.NotNull;
 
-public record Toy(@NotNull String name) {}
+public record Toy(@NotNull String name) {
+    public Toy {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be blank");
+        }
+    }
+}
